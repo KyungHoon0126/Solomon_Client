571025fcf2bbd5367351a629f3d366af66b7fedb
김경훈 <kidi052812@gmail.com>
2020-09-04 20:31:47 +0900

    [ADD] : Client 개발 세팅, TDD 개발을 위한 UnitTestProject 추가, MVVM Base 작업

---
+++ Solomon_Client/Solomon.Core.Member.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solomon.Core.Member.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

59b6891a873df295c256ad36cbe7c9f6609c5c4f
김경훈 <kidi052812@gmail.com>
2020-09-17 20:49:57 +0900

    [ADD] : HomeControl, BulletinControl, OptionControl

---
+++ Solomon_Client/Solomon_Client/Controls/BulletinControl/BulletinControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Solomon_Client.Controls.BulletinControl
+{
+    /// <summary>
+    /// Interaction logic for BulletinControl.xaml
+    /// </summary>
+    public partial class BulletinControl : UserControl, IPage
+    {
+        public const string Title = "Bulletin";
+
+        public BulletinControl()
+        {
+            InitializeComponent();
+        }
+
+        public string GetTitle()
+        {
+            return Title;
+        }
+    }
+}

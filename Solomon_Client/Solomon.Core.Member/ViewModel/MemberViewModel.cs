b9f2e7491bc6439760d5dc72999ef20fe8549440
김경훈 <kidi052812@gmail.com>
2020-10-05 09:43:57 +0900

    [ADD] : MemberViewModel

---
+++ Solomon_Client/Solomon.Core.Member/ViewModel/MemberViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Mvvm;
+using Solomon.Core.Member.Model;
+using Solomon.Core.Member.Service;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Solomon.Core.Member.ViewModel
+{
+    public class MemberViewModel : BindableBase
+    {
+        MemberService memberService = new MemberService();
+
+        public delegate void OnMessageHandler(string msg);
+        public event OnMessageHandler Message;
+
+        #region Properties
+        public MemberModel MemberModel 
+        {
+            get; 
+            set;
+        }
+        #endregion
+
+        public async Task GetMemberInformation(int member_idx)
+        {
+            var resp = await memberService.GetMemberInformation(member_idx);
+
+            if (resp != null && resp.Data != null && resp.Status == 200)
+            {
+                try
+                {
+                    MemberModel.MemberIdx = resp.Data.MemberIdx;
+                    MemberModel.Id = resp.Data.Id;
+                    MemberModel.Name = resp.Data.Name;
+                    MemberModel.Email = resp.Data.Email;
+                }
+                catch (Exception e)
+                {
+                    Message?.Invoke(e.Message);
+                    Debug.WriteLine("GET MEMBER INFORMATION URL ERROR : " + e.Message);
+                }
+            }
+            else
+            {
+                Message?.Invoke(resp.Message);
+            }
+        }
+    }
+}

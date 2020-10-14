b669a42f53955279f29bc028a308b23d310c1679
김경훈 <kidi052812@gmail.com>
2020-10-14 16:06:06 +0900

    [ADD] : ThemesController.cs

--- Solomon_Client/Solomon_Client/Controls/OptionControl/OptionControl.xaml.cs
+++ Solomon_Client/Solomon_Client/Controls/OptionControl/OptionControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Solomon_Client.Common;
+using Solomon_Client.Themes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -60,7 +61,20 @@ namespace Solomon_Client.Controls.OptionControl
 
         private void tscSetDarkMode_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            //MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            //mainWindow.Background = new SolidColorBrush(Color.FromRgb(26, 26, 26));
+        }
 
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            switch (int.Parse(((MenuItem)sender).Uid))
+            {
+                case 0: ThemesController.SetTheme(ThemesController.ThemeTypes.Light); break;
+                case 1: ThemesController.SetTheme(ThemesController.ThemeTypes.ColourfulLight); break;
+                case 2: ThemesController.SetTheme(ThemesController.ThemeTypes.Dark); break;
+                case 3: ThemesController.SetTheme(ThemesController.ThemeTypes.ColourfulDark); break;
+            }
+            e.Handled = true;
         }
     }
 }
